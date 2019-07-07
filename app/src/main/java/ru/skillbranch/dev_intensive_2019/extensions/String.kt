af46def8d5c56a1c043f6c335c001f9610249de7
Oleg Ostrovskyi <ogostrovsky@gmail.com>
2019-07-07 22:00:04 +0300

    v1.12 final

--- app/src/main/java/ru/skillbranch/dev_intensive_2019/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/dev_intensive_2019/extensions/String.kt
@@ -1,2 +1,14 @@
 package ru.skillbranch.dev_intensive_2019.extensions
-fun String.truncate(droper:Int = 16):String{if(this.trim().length > droper) return this.trim().dropLast(this.trim().length-droper).trim()+"..."; return this.trim()}
+
+
+
+fun String.truncate(droper:Int = 16):String{
+    if(this.trim().length > droper) return this.trim().dropLast(this.trim().length-droper).trim()+"..."; return this.trim()
+}
+
+fun String.stripHtml():String {
+    var x = this.replace(Regex("<.*?>" ), " ")
+    x = x.replace(Regex("&.*?;"), "")
+    x = x.replace(Regex("\\s+"), " ")
+    return x.trim()
+}
